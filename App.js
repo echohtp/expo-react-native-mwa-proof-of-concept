9826c0fe2821c460b69922623486dd06089df50e
Jason Schorr <jason@0xbanana.com>
2023-02-01 18:34:13 -0400

    update w disconnect button and different styling

--- App.js
+++ App.js
@@ -1,24 +1,100 @@
 import { transact } from "@solana-mobile/mobile-wallet-adapter-protocol";
-
+import * as NavigationBar from 'expo-navigation-bar';
 import { StatusBar } from "expo-status-bar";
-import { useCallback } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+
+  const [foundAccounts, setFoundAccounts] = useState([])
+  const [authToken, setAuthToken] = useState()
+
   return (
     <View style={styles.container}>
-      <Button
+      {foundAccounts.length > 0 && foundAccounts.map((a, idx) => <Text key={idx}>{a.label}</Text>)}
+
+
+      {/* connect button */}
+      {foundAccounts.length == 0 && 
+      <TouchableOpacity
         onPress={() => {
           transact(async (mobileWallet) => {
+            try{
             const authorization = await mobileWallet.authorize({
               cluster: "devnet",
               identity: { name: "My Expo App" },
             });
+
             console.log(authorization);
+            setFoundAccounts(authorization.accounts)
+            setAuthToken(authorization.auth_token)
+            }catch{
+              
+            }
           });
         }}
-        title="Authorize"
-      />
+        title="Connect Wallet"
+        style={{
+          width: 130,
+          borderRadius: 4,
+          backgroundColor: '#14274e',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 40
+        }}
+      >
+        <Text
+          style={{
+            color: '#fff',
+            fontWeight: 'bold',
+            textAlign: 'center'
+          }}
+        >
+          Connect Wallet
+        </Text>
+      </TouchableOpacity>
+      }
+
+
+      {/* disconnect button */}
+      {foundAccounts.length > 0 && 
+      <TouchableOpacity
+        onPress={() => {
+          transact(async (mobileWallet) => {
+            const authorization = await mobileWallet.deauthorize({
+              auth_token: authToken
+            });
+
+            console.log(authorization);
+            setFoundAccounts([])
+            setAuthToken(null)
+          });
+        }}
+        title="Disconnect"
+        style={{
+          width: 130,
+          borderRadius: 4,
+          backgroundColor: '#14274e',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 40
+        }}
+      >
+        <Text
+          style={{
+            color: '#fff',
+            fontWeight: 'bold',
+            textAlign: 'center'
+          }}
+        >
+          Disconnect
+        </Text>
+      </TouchableOpacity>
+      }
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -31,4 +107,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  heroText: {
+    fontSize: 64
+  }
 });
